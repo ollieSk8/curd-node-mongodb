03e08f45836bde4ff57e9438bc1c2a232f64e56d
ollieSk8 <412537738@qq.com>
2017-03-24 17:51:36 +0800

    add bootstrap

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req, res) => {
     res.render('index');
 });
